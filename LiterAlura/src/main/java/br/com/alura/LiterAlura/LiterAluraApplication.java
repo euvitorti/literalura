e4389138e68a3b7fc94036f6468c433e619c12e5
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-06-09 16:51:30 -0300

    Dependências AuthorRepository e BookRepository injetadas com @Autowired.

--- LiterAlura/src/main/java/br/com/alura/LiterAlura/LiterAluraApplication.java
+++ LiterAlura/src/main/java/br/com/alura/LiterAlura/LiterAluraApplication.java
@@ -1,13 +1,30 @@
 package br.com.alura.LiterAlura;
 
+import br.com.alura.LiterAlura.main.Principal;
+import br.com.alura.LiterAlura.models.author.Author;
+import br.com.alura.LiterAlura.repository.author.AuthorRepository;
+import br.com.alura.LiterAlura.repository.book.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiterAluraApplication {
+public class LiterAluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private AuthorRepository authorRepository;
+
+	@Autowired
+	private BookRepository bookRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(authorRepository, bookRepository);
+        principal.menu();
+	}
 }
