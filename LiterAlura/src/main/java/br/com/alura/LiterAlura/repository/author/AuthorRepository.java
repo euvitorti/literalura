bc4dfac667938d11831dfdd4c21f99324003731f
João Vítor Santos Lima <jvitor.ti03@gmail.com>
2024-06-20 18:32:51 -0300

    Buscar autor por determinado por ano

--- LiterAlura/src/main/java/br/com/alura/LiterAlura/repository/author/AuthorRepository.java
+++ LiterAlura/src/main/java/br/com/alura/LiterAlura/repository/author/AuthorRepository.java
@@ -4,5 +4,9 @@ package br.com.alura.LiterAlura.repository.author;
 import br.com.alura.LiterAlura.models.author.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    List<Author> findAuthorByBirthYear(int year);
 }
